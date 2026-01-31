0f9586dfbc60f69211414e004ee816cc39977d88
venkatasaiKommu <Venkatasai.Kommu@Kroll.com>
2026-01-31 16:22:31 +0530

    addeD

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request, send_from_directory, redirect, url_for, flash
 import random, os
 from werkzeug.utils import secure_filename
-# Temporarily commented out - requires TensorFlow installation
-# from functions import img_predict, get_diseases_classes, get_crop_recommendation, get_fertilizer_recommendation, soil_types, Crop_types, crop_list
+from functions import img_predict, get_diseases_classes, get_crop_recommendation, get_fertilizer_recommendation, soil_types, Crop_types, crop_list
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from database import db, User
 
@@ -101,58 +100,44 @@ def logout():
 @app.route('/crop-recommendation', methods=['GET', 'POST'])
 @login_required
 def crop_recommendation():
-	# Temporarily disabled - requires TensorFlow
-	flash('This feature requires TensorFlow installation. Coming soon!', 'info')
-	return render_template('crop-recommend.html')
-	# if request.method == "POST":
-	# 	to_predict_list = request.form.to_dict()
-	# 	to_predict_list = list(to_predict_list.values())
-	# 	to_predict_list = list(map(float, to_predict_list))
-	# 	result = get_crop_recommendation(to_predict_list)
-	# 	return render_template("recommend_result.html", result=result)
-	# else:
-	# 	return render_template('crop-recommend.html')
+	if request.method == "POST":
+		to_predict_list = request.form.to_dict()
+		to_predict_list = list(to_predict_list.values())
+		to_predict_list = list(map(float, to_predict_list))
+		result = get_crop_recommendation(to_predict_list)
+		return render_template("recommend_result.html", result=result)
+	else:
+		return render_template('crop-recommend.html')
 
-@# Temporarily disabled - requires TensorFlow
-	flash('This feature requires TensorFlow installation. Coming soon!', 'info')
-	soil_types = ['Sandy', 'Loamy', 'Black', 'Red', 'Clayey']
-	Crop_types = ['Maize', 'Sugarcane', 'Cotton', 'Tobacco', 'Paddy', 'Barley', 'Wheat', 'Millets', 'Oil seeds', 'Pulses', 'Ground Nuts']
-	return render_template('fertilizer-recommend.html', soil_types=enumerate(soil_types), crop_types=enumerate(Crop_types))
-	# if request.method == "POST":
-	# 	to_predict_list = request.form.to_dict()
-	# 	to_predict_list = list(to_predict_list.values())
-	# 	to_predict_list = list(map(float, to_predict_list))
-	# 	result = get_fertilizer_recommendation(
-	# 		num_features=to_predict_list[:-2],
-	# 		cat_features=to_predict_list[-2:]
-	# 	)
-	# 	return render_template("recommend_result.html", result=result)
-	# else:
-	# 	return render_template(
-	# 		'fertilizer-recommend.html', 
-	# 		soil_types=enumerate(soil_types),
-	# 		crop_types=enumerate(Crop_types)
-	# 		'fertilizer-recommend.html', 
+@app.route('/fertilizer-recommendation', methods=['GET', 'POST'])
+@login_required
+def fertilizer_recommendation():
+	if request.method == "POST":
+		to_predict_list = request.form.to_dict()
+		to_predict_list = list(to_predict_list.values())
+		to_predict_list = list(map(float, to_predict_list))
+		result = get_fertilizer_recommendation(
+			num_features=to_predict_list[:-2],
+			cat_features=to_predict_list[-2:]
+		)
+		return render_template("recommend_result.html", result=result)
+	else:
+		return render_template(
+			'fertilizer-recommend.html', 
 			soil_types=enumerate(soil_types),
 			crop_types=enumerate(Crop_types)
 		)
-# Temporarily disabled - requires TensorFlow
-	crop_list = ['apple', 'cherry', 'corn', 'grape', 'peach', 'pepper', 'potato', 'strawberry', 'tomato']
-	flash('This feature requires TensorFlow installation. Coming soon!', 'info')
-	return render_template('crop-disease.html', crops=crop_list)
-	# if request.method=="GET":
-	# 	return render_template('crop-disease.html', crops=crop_list)
-	# else:
-	# 	file = request.files["file"]
-	# 	crop = request.form["crop"]
+@app.route('/disease-prediction', methods=['GET', 'POST'])
+@login_required
+def disease_prediction():
+	if request.method=="GET":
+		return render_template('crop-disease.html', crops=crop_list)
+	else:
+		file = request.files["file"]
+		crop = request.form["crop"]
 
-	# 	basepath = os.path.dirname(__file__)
-	# 	file_path = os.path.join(basepath,'uploads',  secure_filename(file.filename))
-	# 	file.save(file_path)
-	# 	prediction = img_predict(file_path, crop)
-	# 	result = get_diseases_classes(crop, prediction)
-
-	# 	file_path = os.path.join(basepath,'uploads',  secure_filename(file.filename))
+		basepath = os.path.dirname(__file__)
+		file_path = os.path.join(basepath,'uploads',  secure_filename(file.filename))
 		file.save(file_path)
 		prediction = img_predict(file_path, crop)
 		result = get_diseases_classes(crop, prediction)
